5ab888d564ee83830340a4ce1805cd77628e493d
tchak <paul@chavard.net>
2012-04-11 00:41:19 +0200

    validations using errors object
    Conflicts:
    
    	packages/ember-data/lib/system/model.js

--- packages/ember-data/lib/system/model.js
+++ packages/ember-data/lib/system/model.js
@@ -3,3 +3,4 @@ require("ember-data/system/model/states");
 require("ember-data/system/model/attributes");
 require("ember-data/system/model/data_proxy");
 require("ember-data/system/model/errors");
+require("ember-data/system/model/validations");
