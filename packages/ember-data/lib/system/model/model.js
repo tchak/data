emberjs/ember.js

Embedded associations need special handling in Model.dataDidChange

lib/system/model/model.js
@@ -296,10 +296,16 @@ DS.Model = Ember.Object.extend(Ember.Evented, {
         if (cachedValue) {
           var key = association.options.key || name,
               ids = data.get(key) || [];
-          var clientIds = Ember.ArrayUtils.map(ids, function(id) {
-            return store.clientIdForId(association.type, id);
-          });
-
+          
+          var clientIds;   
+          if(association.options.embedded) {
+            clientIds = store.loadMany(association.type, ids).clientIds;
+          } else {
+            clientIds = Ember.ArrayUtils.map(ids, function(id) {
+              return store.clientIdForId(association.type, id);
+            });
+          }
+          
           set(cachedValue, 'content', Ember.A(clientIds));
           cachedValue.fetch();
         }

tchak/data

packages/ember-data/lib/system/model/model.js
@@ -296,10 +296,16 @@ DS.Model = Ember.Object.extend(Ember.Evented, {
 
         if (cachedValue) {
           var key = association.options.key || name,
-              ids = data.get(key) || [];
-          var clientIds = Ember.EnumerableUtils.map(ids, function(id) {
-            return store.clientIdForId(association.type, id);
-          });
+              ids = data.get(key) || [],
+              clientIds;
+
+          if (association.options.embedded) {
+            clientIds = store.loadMany(association.type, ids).clientIds;
+          } else {
+            clientIds = Ember.EnumerableUtils.map(ids, function(id) {
+              return store.clientIdForId(association.type, id);
+            });
+          }
 
           set(cachedValue, 'content', Ember.A(clientIds));
           cachedValue.fetch();